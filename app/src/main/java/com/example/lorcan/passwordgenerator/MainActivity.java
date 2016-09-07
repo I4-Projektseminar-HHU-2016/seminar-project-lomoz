f0ae561e15445950f11d0f77c94d001d1ae87d0f
lomoz <lorcan.mozis@hhu.de>
2016-09-07 20:11:45 +0200

    change text in textview by button click

--- app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lorcan.passwordgenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //commentary
+    }
+
+    public void buttonClicked(View v) {
+
+        /*
+        Button button = (Button) v;
+        ((Button) v).setText("clicked");
+        */
+
+        TextView tv = (TextView)findViewById(R.id.textView2);
+        tv.setText("New Password");
     }
 }
