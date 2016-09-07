5211a522e83e6eb88b08d2289aed8bd8c6ed1aa0
lomoz <lorcan.mozis@hhu.de>
2016-09-07 22:27:56 +0200

    booleans to check if checkboxes are clicked

--- app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.lorcan.passwordgenerator;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static boolean uppercaseOn = false;
+    private static boolean lowercaseOn = false;
+    private static boolean numbersOn = false;
+    private static boolean specialsOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +23,43 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClicked(View v) {
 
-        /*
-        Button button = (Button) v;
-        ((Button) v).setText("clicked");
-        */
-
         TextView tv = (TextView)findViewById(R.id.textView2);
         tv.setText("New Password");
+    }
+
+    public void kleinbuchstabenChecked(View v) {
+        //code to check if this checkbox is checked!
+        CheckBox checkBox = (CheckBox)v;
+        if(checkBox.isChecked()){
+
+            lowercaseOn = true;
+        }
+    }
+
+    public void großbuchstabenChecked(View v) {
+        //code to check if this checkbox is checked!
+        CheckBox checkBox = (CheckBox)v;
+        if(checkBox.isChecked()){
+
+            uppercaseOn = true;
+        }
+    }
+
+    public void numbersChecked(View v) {
+        //code to check if this checkbox is checked!
+        CheckBox checkBox = (CheckBox)v;
+        if(checkBox.isChecked()){
+
+            numbersOn = true;
+        }
+    }
+
+    public void specialsChecked(View v) {
+        //code to check if this checkbox is checked!
+        CheckBox checkBox = (CheckBox)v;
+        if(checkBox.isChecked()){
+
+            specialsOn = true;
+        }
     }
 }
