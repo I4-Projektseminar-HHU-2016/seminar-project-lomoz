c569eeccaa7180a0802e7771b7e9febbd71dc3cc
lomoz <lorcan.mozis@hhu.de>
2016-09-10 00:11:37 +0200

    Added uppercase, lowercase, numbers & special characters as instance variables

--- app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
+++ app/src/main/java/com/example/lorcan/passwordgenerator/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private static boolean numbersOn = false;
     private static boolean specialsOn = false;
 
+    private static String[] uppercaseStrings = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private static String[] lowercaseStrings = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    private static String[] numbersStrings = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private static String[] specialsStrings = {"!", "?", "@", "(", ")", "{", "}", "[", "]", "/", "=", "~", "$", "%", "&", "#", "*", "-"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
